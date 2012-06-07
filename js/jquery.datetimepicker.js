7763b50827008e45e92a96f48c2c76b769505105
Maciej Kaniewski <maciej.kaniewski@gmail.com>
2012-06-07 17:25:42 +0100

    connected time inputs with sliders

--- js/jquery.datetimepicker.js
+++ js/jquery.datetimepicker.js
@@ -8,14 +8,30 @@ function zeroFill(number, width) {
 
 function hour(i) {
 	if (i == null)
-		return parseInt(jQuery('#' + _id + " #hour").val());
-	jQuery('#' + _id + " #hour").val(zeroFill(parseInt(i), 2));
+		return jQuery('#' + _id + " #hour").val();
+	
+	var pi;
+	//try to parse input
+	pi = parseInt(i);
+	//TODO hardcoded value
+	if (isNaN(pi) || pi > 23 || pi < 0)
+		//TODO hardcoded value
+		pi = 12;
+	
+	jQuery('#' + _id + " #hour").val(zeroFill(pi, 2));
 	
 }
 function min(i) {
 	if (i == null)
 		return parseInt(jQuery('#' + _id + " #min").val());
-	jQuery('#' + _id + " #min").val(zeroFill(parseInt(i), 2));
+	var pi;
+	//try to parse input
+	pi = parseInt(i);
+	//TODO hardcoded value
+	if (isNaN(pi) || pi > 59 || pi < 0)
+		//TODO hardcoded value
+		pi = 0;
+	jQuery('#' + _id + " #min").val(zeroFill(pi, 2));
 }
 
 // id of element
@@ -54,18 +70,22 @@ function _create_datetimepicker() {
 			$('<div/>').addClass('ui-custom-datetimepicker-hour-pane ui-datepicker-inline ui-datepicker ui-widget  ui-helper-clearfix ui-corner-right').append(
 				$('<div/>').addClass('ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all ui-custom-datetimepicker-header').append(
 					$('<div/>').addClass('').append(
-						$('<input id="hour" type="text" maxlength="2" value="12" />').addClass('ui-widget ui-state-default ui-helper-clearfix ui-corner-all')).append(':').append(
-						$('<input id="min" type="text" maxlength="2" value="12" />').addClass('ui-widget ui-state-default ui-helper-clearfix ui-corner-all')))).append(
+						$('<input id="hour" type="text" maxlength="2" />').addClass('ui-widget ui-state-default ui-helper-clearfix ui-corner-all')).append(':').append(
+						$('<input id="min" type="text" maxlength="2" />').addClass('ui-widget ui-state-default ui-helper-clearfix ui-corner-all')))).append(
 				$('<div/>').addClass('sliders-panel').append(
 					$('<div/>').append('<div id="hour-slider" />')).append(
 					$('<div/>').append('<div id="min-slider" />')))))
 	$('body').append(dp);
+	
 	return dp;
 }
 
 function init_sliders(id, input) {
 	_create_datetimepicker();
 	_setId(id);
+	
+	hour(12);
+	min(0);
 	
 	jQuery('#' + id + ' .datepicker').datepicker();
 	//jQuery('.custom-datetimepicker-hour-pane').height(jQuery('.datepicker .ui-datepicker').height())
@@ -75,24 +95,43 @@ function init_sliders(id, input) {
 		range : "min",
 		min : 0,
 		max : 23,
-		value : 18,
+		value : hour(),
 		slide : function (event, ui) {
 			hour(ui.value)
 		}
 	});
+	
 	jQuery('#' + id + " #min-slider").slider({
 		orientation : "vertical",
 		range : "min",
 		min : 0,
 		max : 59,
-		value : 30,
+		value : min(),
 		slide : function (event, ui) {
 			min(ui.value)
 		}
 	});
+	//join time input fields to sliders
+	$('#hour').focusout(function () {
+		hour(hour());
+		jQuery('#' + id + " #hour-slider").slider('value',hour())
+		
+	})
+	$('#min').focusout(function () {
+		min(min());
+		jQuery('#' + id + " #min-slider").slider('value',min())
+	})
+	// double click increase by one both time and hour
+	$('#min').dblclick(function () {
+		min(parseInt(min())+1)
+		jQuery('#' + id + " #min-slider").slider('value',min())
+	})
+	$('#hour').dblclick(function () {
+		hour(parseInt(hour())+1);
+		jQuery('#' + id + " #hour-slider").slider('value',hour())
+		
+	})
 	
-	hour(jQuery('#' + id + " #hour-slider").slider('value'));
-	min(jQuery('#' + id + " #min-slider").slider('value'));
 	
 	var dialog = jQuery('#' + id).dialog({
 			dialogClass : 'notitle',
@@ -106,51 +145,27 @@ function init_sliders(id, input) {
 			resizable : false,
 			draggable : false,
 			autoOpen : false
-		})
-		
-		/*jQuery(_buttonPane)
-		.append(
-		jQuery('<input/>')
-		.attr('id', 'datejs-input')
-		.addClass("ui-widget ui-state-default ui-corner-left ui-custom-datetimepicker-datejs-input"));
-		
-		jQuery(_buttonPane).append(
-		jQuery('<span/>')
-		.attr('id', 'datejs-refresh')
-		.addClass("ui-widget ui-state-default ui-corner-right ui-custom-datetimepicker-refresh-icon")
-		.append(
-		jQuery('<span>refresh</span>')
-		.addClass("ui-icon ui-icon-refresh")));
-		jQuery(_buttonPane + ' #datejs-refresh')
-		.mouseenter(function () {
-		jQuery(_buttonPane + ' #datejs-refresh')
-		.removeClass('ui-state-default')
-		.addClass('ui-state-hover')
-		})
-		.mouseleave(function () {
-		jQuery(_buttonPane + ' #datejs-refresh')
-		.removeClass('ui-state-hover')
-		.addClass('ui-state-default')
-		});*/
-		jQuery(_buttonPane)
-		.append(
-			'<span class="ui-custom-datetimepicker-datejs-input"><input class="ui-state-default ui-widget ui-widget-content ui-corner-left" ><a title="Show All Items" class="ui-button ui-widget ui-state-default ui-button-icon-only ui-corner-right ui-button-icon" role="button" aria-disabled="false"><span class="ui-button-icon-primary ui-icon ui-icon ui-icon-refresh"></span><span class="ui-button-text"></span></a></span>')
-		
-		dialog.parent().mouseenter(function () {
-			_mouseover.dialog = true;
-		}).mouseleave(function () {
-			_mouseover.dialog = false;
-		})
-		$(input).mouseenter(function () {
-			_mouseover.input = true;
-		}).mouseleave(function () {
-			_mouseover.input = false;
-		})
-		
-		jQuery(document).click(function () {
-			if (!_mouseover.dialog && !_mouseover.input)
-				dialog.dialog('close');
 		});
+	
+	jQuery(_buttonPane)
+	.append(
+		'<span class="ui-custom-datetimepicker-datejs-input"><input class="ui-state-default ui-widget ui-widget-content ui-corner-left" ><a title="Show All Items" class="ui-button ui-widget ui-state-default ui-button-icon-only ui-corner-right ui-button-icon" role="button" aria-disabled="false"><span class="ui-button-icon-primary ui-icon ui-icon ui-icon-refresh"></span><span class="ui-button-text"></span></a></span>')
+	
+	dialog.parent().mouseenter(function () {
+		_mouseover.dialog = true;
+	}).mouseleave(function () {
+		_mouseover.dialog = false;
+	});
+	$(input).mouseenter(function () {
+		_mouseover.input = true;
+	}).mouseleave(function () {
+		_mouseover.input = false;
+	});
+	
+	jQuery(document).click(function () {
+		if (!_mouseover.dialog && !_mouseover.input)
+			dialog.dialog('close');
+	});
 	
 	$(input).click(function () {
 		dialog.dialog('open');
@@ -159,6 +174,6 @@ function init_sliders(id, input) {
 			at : 'left bottom',
 			of : $(input)
 		});
-	})
+	});
 	
 }
