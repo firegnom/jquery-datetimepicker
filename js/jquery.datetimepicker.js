0a3421069a3c88f0d5887d99ab1088ab3b20ac50
Maciej Kaniewski <maciej.kaniewski@gmail.com>
2012-06-07 16:31:38 +0100

    mostly working ui

--- js/jquery.datetimepicker.js
+++ js/jquery.datetimepicker.js
@@ -52,23 +52,23 @@ function _create_datetimepicker() {
 	dp.append(
 		$('<div/>').addClass('timepicker').append(
 			$('<div/>').addClass('ui-custom-datetimepicker-hour-pane ui-datepicker-inline ui-datepicker ui-widget  ui-helper-clearfix ui-corner-right').append(
-				$('<div/>').addClass('ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all').append(
-					$('<div/>').addClass('ui-custom-datetimepicker-header').append(
+				$('<div/>').addClass('ui-datepicker-header ui-widget-header ui-helper-clearfix ui-corner-all ui-custom-datetimepicker-header').append(
+					$('<div/>').addClass('').append(
 						$('<input id="hour" type="text" maxlength="2" value="12" />').addClass('ui-widget ui-state-default ui-helper-clearfix ui-corner-all')).append(':').append(
 						$('<input id="min" type="text" maxlength="2" value="12" />').addClass('ui-widget ui-state-default ui-helper-clearfix ui-corner-all')))).append(
 				$('<div/>').addClass('sliders-panel').append(
-					$('<div/>').append('<div id="hour-slider" style="height:125px; margin:1em;"></div>')).append(
-					$('<div/>').append('<div id="min-slider" style="height:125px;margin:1em;"></div>')))))
+					$('<div/>').append('<div id="hour-slider" />')).append(
+					$('<div/>').append('<div id="min-slider" />')))))
 	$('body').append(dp);
 	return dp;
 }
 
 function init_sliders(id, input) {
 	_create_datetimepicker();
+	_setId(id);
 	
-	_setId(id)
 	jQuery('#' + id + ' .datepicker').datepicker();
-	//	jQuery('.custom-datetimepicker-hour-pane').height(jQuery('.datepicker .ui-datepicker').height())
+	//jQuery('.custom-datetimepicker-hour-pane').height(jQuery('.datepicker .ui-datepicker').height())
 	
 	jQuery('#' + id + " #hour-slider").slider({
 		orientation : "vertical",
@@ -105,58 +105,59 @@ function init_sliders(id, input) {
 			width : jQuery('#' + id + ' .datepicker').width() + jQuery('#' + id + ' .timepicker').width(),
 			resizable : false,
 			draggable : false,
-			autoOpen : false,
-			title : null
-			//height: ''
+			autoOpen : false
 		})
 		
-		jQuery(_buttonPane)
+		/*jQuery(_buttonPane)
 		.append(
-			jQuery('<input/>')
-			.attr('id', 'datejs-input')
-			.addClass("ui-widget ui-state-default ui-corner-left ui-custom-datetimepicker-datejs-input"));
-	
-	jQuery(_buttonPane).append(
+		jQuery('<input/>')
+		.attr('id', 'datejs-input')
+		.addClass("ui-widget ui-state-default ui-corner-left ui-custom-datetimepicker-datejs-input"));
+		
+		jQuery(_buttonPane).append(
 		jQuery('<span/>')
 		.attr('id', 'datejs-refresh')
 		.addClass("ui-widget ui-state-default ui-corner-right ui-custom-datetimepicker-refresh-icon")
 		.append(
-			jQuery('<span>refresh</span>')
-			.addClass("ui-icon ui-icon-refresh")));
-	jQuery(_buttonPane + ' #datejs-refresh')
-	.mouseenter(function () {
+		jQuery('<span>refresh</span>')
+		.addClass("ui-icon ui-icon-refresh")));
+		jQuery(_buttonPane + ' #datejs-refresh')
+		.mouseenter(function () {
 		jQuery(_buttonPane + ' #datejs-refresh')
 		.removeClass('ui-state-default')
 		.addClass('ui-state-hover')
-	})
-	.mouseleave(function () {
+		})
+		.mouseleave(function () {
 		jQuery(_buttonPane + ' #datejs-refresh')
 		.removeClass('ui-state-hover')
 		.addClass('ui-state-default')
-	});
-	
-	dialog.parent().mouseenter(function () {
-		_mouseover.dialog = true;
-	}).mouseleave(function () {
-		_mouseover.dialog = false;
-	})
-	$(input).mouseenter(function () {
-		_mouseover.input = true;
-	}).mouseleave(function () {
-		_mouseover.input = false;
-	})
-	
-	jQuery(document).click(function () {
-		if (!_mouseover.dialog && !_mouseover.input)
-			dialog.dialog('close');
-	});
+		});*/
+		jQuery(_buttonPane)
+		.append(
+			'<span class="ui-custom-datetimepicker-datejs-input"><input class="ui-state-default ui-widget ui-widget-content ui-corner-left" ><a title="Show All Items" class="ui-button ui-widget ui-state-default ui-button-icon-only ui-corner-right ui-button-icon" role="button" aria-disabled="false"><span class="ui-button-icon-primary ui-icon ui-icon ui-icon-refresh"></span><span class="ui-button-text"></span></a></span>')
+		
+		dialog.parent().mouseenter(function () {
+			_mouseover.dialog = true;
+		}).mouseleave(function () {
+			_mouseover.dialog = false;
+		})
+		$(input).mouseenter(function () {
+			_mouseover.input = true;
+		}).mouseleave(function () {
+			_mouseover.input = false;
+		})
+		
+		jQuery(document).click(function () {
+			if (!_mouseover.dialog && !_mouseover.input)
+				dialog.dialog('close');
+		});
 	
 	$(input).click(function () {
 		dialog.dialog('open');
 		dialog.parent().position({
-			my : 'left bottom',
-			at : 'left top',
-			of : 'div[name=input]>input:first'
+			my : 'left top',
+			at : 'left bottom',
+			of : $(input)
 		});
 	})
 	
